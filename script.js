88937f49bfdf29f317cf6922c169f558eda26001
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-12-06 19:36:36 +0530

    React1

--- script.js
+++
@@ -1,6 +0,0 @@
-async function fetchApi() {
-    const responce = await fetch("https://api.postalpincode.in/pincode/110063")
-    const data = await responce.json()
-    console.log(data[0].PostOffice)
-}
-fetchApi()
